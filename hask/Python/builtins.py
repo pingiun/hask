94f459fe5170ec873767ed59910f05123aeec686
Manuel Vázquez Acosta <manuel@merchise.org>
2018-07-16 17:56:42 -0400

    Use a *standard* future imports.

--- hask/Python/builtins.py
+++ hask/Python/builtins.py
@@ -1,3 +1,7 @@
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
 from ..lang import H
 
 try:
--- hask/lang/hindley_milner.py
+++ hask/lang/hindley_milner.py
@@ -1,3 +1,7 @@
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
 # Implementation of Hindley-Milner type inference system for Python, based on
 # Robert Smallshire's implementation for OWL BASIC.
 #
