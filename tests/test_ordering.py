f624c59ac66e6388ebc3ce5c2b27bea914049108
Manuel Vázquez Acosta <manuel@merchise.org>
2018-07-16 12:11:12 -0400

    Extract Ordering tests

---
+++ tests/test_ordering.py
@@ -0,0 +1,38 @@
+import unittest
+
+from hask import has_instance
+from hask import Read, Show, Eq, Ord, Bounded
+
+
+class TestOrdering(unittest.TestCase):
+
+    def test_ordering(self):
+        from hask.Data.Ord import Ordering, LT, EQ, GT
+        self.assertTrue(has_instance(Ordering, Read))
+        self.assertTrue(has_instance(Ordering, Show))
+        self.assertTrue(has_instance(Ordering, Eq))
+        self.assertTrue(has_instance(Ordering, Ord))
+        self.assertTrue(has_instance(Ordering, Bounded))
+
+        from hask.Prelude import show
+        self.assertEqual("LT", show(LT))
+        self.assertEqual("EQ", show(EQ))
+        self.assertEqual("GT", show(GT))
+
+        self.assertTrue(EQ == EQ and not EQ != EQ)
+        self.assertTrue(LT == LT and not LT != LT)
+        self.assertTrue(GT == GT and not GT != GT)
+        self.assertFalse(LT == EQ)
+        self.assertFalse(LT == GT)
+        self.assertFalse(EQ == GT)
+        self.assertTrue(LT < EQ < GT)
+        self.assertTrue(LT <= EQ < GT)
+        self.assertTrue(LT < EQ <= GT)
+        self.assertTrue(LT <= EQ <= GT)
+        self.assertFalse(LT > EQ or EQ > GT)
+        self.assertFalse(LT >= EQ or EQ > GT)
+        self.assertFalse(LT > EQ or EQ >= GT)
+        self.assertFalse(LT >= EQ or EQ >= GT)
+
+        with self.assertRaises(TypeError):
+            EQ + EQ
