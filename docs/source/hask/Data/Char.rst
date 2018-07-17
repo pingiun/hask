20ef9d689c0d07899592f51c928033720a647f42
Manuel Vázquez Acosta <manuel@merchise.org>
2018-07-17 18:04:02 -0400

    Document module hask.Data.Char

--- docs/source/hask/Data/Char.rst
+++ docs/source/hask/Data/Char.rst
@@ -3,4 +3,65 @@
 ============================================
 
 .. automodule:: hask.Data.Char
-   :members:
+
+.. autofunction:: isControl
+
+.. autofunction:: isSpace
+
+.. autofunction:: isLower
+
+.. autofunction:: isUpper
+
+.. autofunction:: isAlpha
+
+.. autofunction:: isAlphaNum
+
+.. autofunction:: isPrint
+
+.. autofunction:: isDigit
+
+.. autofunction:: isOctDigit
+
+.. autofunction:: isHexDigit
+
+.. autofunction:: isLetter
+
+.. autofunction:: isMark
+
+.. autofunction:: isNumber
+
+.. autofunction:: isPunctuation
+
+.. autofunction:: isSymbol
+
+.. autofunction:: isSeparator
+
+.. autofunction:: isAscii
+
+.. autofunction:: isLatin1
+
+.. autofunction:: isAsciiUpper
+
+.. autofunction:: isAsciiLower
+
+.. autofunction:: toLower
+
+.. autofunction:: toUpper
+
+.. autofunction:: toTitle
+
+.. autofunction:: digitToInt
+
+.. autofunction:: intToDigit
+
+
+.. function:: chr(x)
+
+   The builtin `chr` converted to a `~hask.lang.type_system.TypedFunc`:class:.
+   Defined as ``chr ** (H/ int >> str)``
+
+
+.. function:: ord(x)
+
+   The builtin `ord` converted to a `~hask.lang.type_system.TypedFunc`:class:.
+   Defined as ``ord ** (H/ int >> str)``
--- hask/Data/Char.py
+++ hask/Data/Char.py
@@ -4,267 +4,267 @@ from ..lang import sig
 
 @sig(H/ str >> bool)
 def isControl(s):
-    """
-    isControl :: str -> bool
+    """``isControl :: str -> bool``
 
     Selects control characters, which are the non-printing characters of the
     Latin-1 subset of Unicode.
+
     """
     raise NotImplementedError
 
 
 @sig(H/ str >> bool)
 def isSpace(s):
-    """
-    isSpace :: str -> bool
+    r"""``isSpace :: str -> bool``
 
     Returns True for any Unicode space character, and the control characters
     \t, \n, \r, \f, \v.
+
     """
     return s in " \t\n\r\f\v"
 
 
 @sig(H/ str >> bool)
 def isLower(s):
-    """
-    isLower :: str -> bool
+    """``isLower :: str -> bool``
 
     Selects lower-case alphabetic Unicode characters (letters).
+
     """
     return s.lower() == s
 
 
 @sig(H/ str >> bool)
 def isUpper(s):
-    """
-    isUpper :: str -> bool
+    """``isUpper :: str -> bool``
 
     Selects upper-case or title-case alphabetic Unicode characters (letters).
     Title case is used by a small number of letter ligatures like the
     single-character form of Lj.
+
     """
     return s.upper() == s
 
 
 @sig(H/ str >> bool)
 def isAlpha(s):
-    """
-    isAlpha :: str -> bool
+    """``isAlpha :: str -> bool``
 
     Selects alphabetic Unicode characters (lower-case, upper-case and
     title-case letters, plus letters of caseless scripts and modifiers
     letters). This function is equivalent to isLetter.
+
     """
     raise NotImplementedError
 
 
 @sig(H/ str >> bool)
 def isAlphaNum(s):
-    """
-    isAlphaNum :: str -> bool
+    """``isAlphaNum :: str -> bool``
 
     Selects alphabetic or numeric digit Unicode characters.
 
     Note that numeric digits outside the ASCII range are selected by this
     function but not by isDigit. Such digits may be part of identifiers but are
     not used by the printer and reader to represent numbers.
+
     """
     raise NotImplementedError
 
 
 @sig(H/ str >> bool)
 def isPrint(s):
-    """
-    isPrint :: str -> bool
+    """``isPrint :: str -> bool``
 
     Selects printable Unicode characters (letters, numbers, marks, punctuation,
     symbols and spaces).
+
     """
     raise NotImplementedError
 
 
 @sig(H/ str >> bool)
 def isDigit(s):
-    """
-    isDigit :: str -> bool
+    """``isDigit :: str -> bool``
 
     Selects ASCII digits, i.e. '0'..'9'.
+
     """
     return s in "0123456789"
 
 
 @sig(H/ str >> bool)
 def isOctDigit(s):
-    """
-    isOctDigit :: str -> bool
+    """``isOctDigit :: str -> bool``
 
     Selects ASCII octal digits, i.e. '0'..'7'.
+
     """
     return s in "01234567"
 
 
 @sig(H/ str >> bool)
 def isHexDigit(s):
-    """
-    isHexDigit :: str -> bool
+    """``isHexDigit :: str -> bool``
 
     Selects ASCII hexadecimal digits, i.e. '0'..'9', 'a'..'f', 'A'..'F'.
+
     """
     return s in "0123456789abcdefABCDEF"
 
 
 @sig(H/ str >> bool)
 def isLetter(s):
-    """
-    isLetter :: str -> bool
+    """``isLetter :: str -> bool``
 
     Selects alphabetic Unicode characters (lower-case, upper-case and
     title-case letters, plus letters of caseless scripts and modifiers
     letters). This function is equivalent to isAlpha.
+
     """
     return isAlpha(s)
 
 
 @sig(H/ str >> bool)
 def isMark(s):
-    """
-    isMark :: str -> bool
+    """``isMark :: str -> bool``
 
     Selects Unicode mark characters, e.g. accents and the like, which combine
     with preceding letters.
+
     """
     raise NotImplementedError
 
 
 @sig(H/ str >> bool)
 def isNumber(s):
-    """
-    isNumber :: str -> bool
+    """``isNumber :: str -> bool``
 
     Selects Unicode numeric characters, including digits from various scripts,
     Roman numerals, etc.
+
     """
     raise NotImplementedError
 
 
 @sig(H/ str >> bool)
 def isPunctuation(s):
-    """
-    isPunctuation :: str -> bool
+    """``isPunctuation :: str -> bool``
 
     Selects Unicode punctuation characters, including various kinds of
     connectors, brackets and quotes.
+
     """
     raise NotImplementedError
 
 
 @sig(H/ str >> bool)
 def isSymbol(s):
-    """
-    isSymbol :: str -> bool
+    """``isSymbol :: str -> bool``
 
     Selects Unicode symbol characters, including mathematical and currency
     symbols.
+
     """
     raise NotImplementedError
 
 
 @sig(H/ str >> bool)
 def isSeparator(s):
-    """
-    isSeparator :: str -> bool
+    """``isSeparator :: str -> bool``
 
     Selects Unicode space and separator characters.
+
     """
     raise NotImplementedError
 
 
 @sig(H/ str >> bool)
 def isAscii(s):
-    """
-    isAscii :: str -> bool
+    """``isAscii :: str -> bool``
 
     Selects the first 128 characters of the Unicode character set,
     corresponding to the ASCII character set.
+
     """
     return ord(s) < 128
 
 
 @sig(H/ str >> bool)
 def isLatin1(s):
-    """
-    isLatin1 :: str -> bool
+    """``isLatin1 :: str -> bool``
 
     Selects the first 256 characters of the Unicode character set,
     corresponding to the ISO 8859-1 (Latin-1) character set.
+
     """
     return ord(s) < 256
 
 
 @sig(H/ str >> bool)
 def isAsciiUpper(s):
-    """
-    isAsciiUpper :: str -> bool
+    """``isAsciiUpper :: str -> bool``
 
     Selects ASCII upper-case letters, i.e. characters satisfying both isAscii
     and isUpper.
+
     """
     return isAscii(s) and isUpper(s)
 
 
 @sig(H/ str >> bool)
 def isAsciiLower(s):
-    """
-    isAsciiLower :: str -> bool
+    """``isAsciiLower :: str -> bool``
 
     Selects ASCII lower-case letters, i.e. characters satisfying both isAscii
     and isLower.
+
     """
     return isAscii(s) and isLower(s)
 
 
 @sig(H/ str >> str)
 def toUpper(s):
-    """
-    toUpper :: str -> str
+    """``toUpper :: str -> str``
 
     Convert a letter to the corresponding upper-case letter, if any. Any other
     character is returned unchanged.
+
     """
     return s.upper()
 
 
 @sig(H/ str >> str)
 def toLower(s):
-    """
-    toLower :: str -> str
+    """``toLower :: str -> str``
 
     Convert a letter to the corresponding lower-case letter, if any. Any other
     character is returned unchanged.
+
     """
     return s.lower()
 
 
 @sig(H/ str >> str)
 def toTitle(s):
-    """
-    toTitle :: str -> str
+    """``toTitle :: str -> str``
 
     Convert a letter to the corresponding title-case or upper-case letter, if
     any. (Title case differs from upper case only for a small number of
     ligature letters.) Any other character is returned unchanged.
+
     """
     return toUpper(s)
 
 
 @sig(H/ str >> int)
 def digitToInt(s):
-    """
-    digitToInt :: str -> int
+    """``digitToInt :: str -> int``
 
     Convert a single digit Char to the corresponding Int. This function fails
     unless its argument satisfies isHexDigit, but recognises both upper and
     lower-case hexadecimal digits (i.e. '0'..'9', 'a'..'f', 'A'..'F').
+
     """
     if s not in "0123456789abcdefABCDEF":
         raise ValueError("not a digit %s" % s)
@@ -273,12 +273,12 @@ def digitToInt(s):
 
 @sig(H/ int >> str)
 def intToDigit(s):
-    """
-    intToDigit :: int -> str
+    """``intToDigit :: int -> str``
 
     Convert an Int in the range 0..15 to the corresponding single digit Char.
     This function fails on other inputs, and generates lower-case hexadecimal
     digits.
+
     """
     if s > 15 or s < 0:
         raise ValueError("not a digit %s" % s)
