97f076b962eeea45bdab7a2da542fbfe20b1f4ad
Medardo Rodriguez <med@merchise.org>
2018-08-09 09:12:26 -0400

    Add new decorator `objectify`.
    This construction must go in `xoutil.decorator`, and -maybe- deprecate
    `instantiate`.

--- hask/hack.py
+++ hask/hack.py
@@ -16,9 +16,10 @@ for example `xoutil`.
 
 from __future__ import division, print_function, absolute_import
 
+from xoutil.decorator.meta import decorator
+
 
 # Utilities
-
 
 def safe_issubclass(cls, class_or_tuple):
     from inspect import isclass
@@ -28,3 +29,41 @@ def safe_issubclass(cls, class_or_tuple):
 def isin(a, b):
     '''Same as ``a in b`` but using ``is`` operator to compare.'''
     return any(a is item for item in b)
+
+
+# TODO: Next construction must go in `xoutil.decorator`, and -maybe- deprecate
+# `instantiate`.
+
+@decorator
+def objectify(target, *args, **kwargs):
+    '''Instantiate a class returning a singleton object.
+
+    Every argument, positional or keyword, is passed as such when invoking the
+    target to create the resulting object.  The following code sample show
+    this::
+
+      >>> @objectify(name='bar')
+      ... class foo(object):
+      ...     def __init__(self, name='<empty>'):
+      ...         self.name = name
+      ...
+      ...     def __str__(self):
+      ...         res = '"{}" instance named "{}"'
+      ...         return res.format(type(self).__name__, self.name)
+      ...
+      ...     __repr__ = __str__
+
+      >>> foo
+      '"foo" instance named "bar"'
+
+      >>> isinstance(foo, type)
+      False
+
+    New Python 3 ``super`` style could be used in these definitions without
+    any problem; but take special care in Python 2, we advise direct call of
+    the base class method (``BaseClass.method(self, ...)``), or calculating
+    the type (``super(type(self), self).method(...)``).  Watch the second case
+    if you inherits from the class like in ``class SubFoo(type(foo))``.
+
+    '''
+    return target(*args, **kwargs)
