752280e07f0f223cdbaae87ed7663171bdf1ddfa
Medardo Rodriguez <med@merchise.org>
2018-08-10 16:57:14 -0400

    Sort 'Data.Maybe' module imports.

--- hask/Data/Maybe.py
+++ hask/Data/Maybe.py
@@ -1,60 +1,73 @@
 from __future__ import division, print_function, absolute_import
 
-from ..lang import Read
-from ..lang import Show
-from ..lang import L
-from ..lang import H
-from ..lang import sig
-from ..lang import t
-from ..lang import data
-from ..lang import d
-from ..lang import caseof
-from ..lang import m
-from ..lang import p
-from ..lang import deriving
-from ..lang import instance
-from ..lang import typify
+from hask.lang.typeclasses import Show
+from hask.lang.typeclasses import Read
 
-from .Eq import Eq
-from .Ord import Ord
-from .Functor import Functor
-from ..Control.Applicative import Applicative
-from ..Control.Monad import Monad
+from hask.lang.syntax import H
+from hask.lang.syntax import sig
+from hask.lang.syntax import t
+
+from hask.lang.syntax import data
+from hask.lang.syntax import d
+from hask.lang.syntax import deriving
+from hask.lang.syntax import instance
+
+from hask.Data.Eq import Eq
+from hask.Data.Ord import Ord
+from hask.Data.Functor import Functor
+from hask.Control.Applicative import Applicative
+from hask.Control.Monad import Monad
 
 
 # data Maybe a = Nothing | Just a deriving(Show, Eq, Ord)
 Maybe, Nothing, Just =\
     data.Maybe("a") == d.Nothing | d.Just("a") & deriving(Read, Show, Eq, Ord)
 
+
+def _fmap(f, x):
+    from hask.lang.syntax import caseof, m, p
+    return ~(caseof(x)
+                | m(Just(m.a)) >> Just(f(p.a))
+                | m(Nothing)   >> Nothing)
+
+
 instance(Functor, Maybe).where(
-    fmap = lambda f, x: ~(caseof(x)
-                            | m(Just(m.a)) >> Just(f(p.a))
-                            | m(Nothing)   >> Nothing)
+    fmap = _fmap
 )
+
 
 instance(Applicative, Maybe).where(
     pure = Just
 )
 
+
+def _bind(x, f):
+    from hask.lang.syntax import caseof, m, p
+    return ~(caseof(x)
+                | m(Just(m.a)) >> f(p.a)
+                | m(Nothing)   >> Nothing)
+
+
 instance(Monad, Maybe).where(
-    bind = lambda x, f: ~(caseof(x)
-                            | m(Just(m.a)) >> f(p.a)
-                            | m(Nothing)   >> Nothing)
+    bind = _bind
 )
 
 
 def in_maybe(fn):
     """Decorator for monadic error handling.
 
-    If the decorated function raises an exception, return Nothing.  Otherwise,
-    take the result and wrap it in a Just.
+    If the decorated function raises an exception, return `Nothing`.
+    Otherwise, take the result and wrap it in a `Just`.
 
     """
+    from hask.lang.syntax import typify, t
+
     def closure_in_maybe(*args, **kwargs):
         try:
             return Just(fn(*args, **kwargs))
         except:    # noqa
             return Nothing
+
     return typify(fn, hkt=lambda x: t(Maybe, x))(closure_in_maybe)
 
 
@@ -62,9 +75,9 @@ def in_maybe(fn):
 def maybe(default, f, maybe_a):
     """``maybe :: b -> (a -> b) -> Maybe a -> b``
 
-    The maybe function takes a default value, a function, and a Maybe value.
-    If the Maybe value is Nothing, the function returns the default value.
-    Otherwise, it applies the function to the value inside the Just and
+    Takes a `default` value, a function `f`, and a `Maybe` value.  If the
+    `Maybe` value is `Nothing`, the function returns the default value.
+    Otherwise, it applies the function to the value inside the `Just` and
     returns the result.
 
     """
@@ -79,7 +92,8 @@ def isJust(a):
 
 @sig(H/ t(Maybe, "a")  >> bool)
 def isNothing(a):
-    '``isNothing :: [Maybe a] -> bool``'
+    '''``isNothing :: [Maybe a] -> bool``'''
+    from hask.lang.syntax import caseof, m
     return ~(caseof(a)
                 | m(Nothing)   >> True
                 | m(Just(m.x)) >> False)
@@ -90,12 +104,14 @@ def fromJust(x):
     '``fromJust :: [Maybe a] -> a``'
     if isJust(x):
         return x[0]
-    raise ValueError("Cannot call fromJust on Nothing.")
+    else:
+        raise ValueError("Cannot call fromJust on Nothing.")
 
 
 @sig(H/ ["a"] >> t(Maybe, "a"))
 def listToMaybe(a):
-    '``listToMaybe :: [a] -> [Maybe a]``'
+    '''``listToMaybe :: [a] -> [Maybe a]``'''
+    from hask.lang.syntax import caseof, m, p
     return ~(caseof(a)
                 | m(m.a ^ m.b) >> Just(p.a)
                 | m(m.a)       >> Nothing)
@@ -105,10 +121,11 @@ def listToMaybe(a):
 def maybeToList(a):
     """``maybeToList :: Maybe a -> [a]``
 
-    The maybeToList function returns an empty list when given Nothing or a
-    singleton list when not given Nothing.
+    Returns an empty list when given `Nothing` or a singleton list when not.
 
     """
+    from hask.lang.syntax import caseof, m, p
+    from hask.lang.lazylist import L
     return ~(caseof(a)
                 | m(Nothing)   >> L[[]]
                 | m(Just(m.x)) >> L[[p.x]])
@@ -118,10 +135,10 @@ def maybeToList(a):
 def catMaybes(a):
     """``catMaybes :: [Maybe a] -> [a]``
 
-    The catMaybes function takes a list of Maybes and returns a list of all the
-    Just values.
+    Takes a list of `Maybes` and returns a list of all the `Just` values.
 
     """
+    from hask.lang.lazylist import L
     return L[(fromJust(item) for item in a if isJust(item))]
 
 
@@ -129,10 +146,16 @@ def catMaybes(a):
 def mapMaybe(f, la):
     """``mapMaybe :: (a -> Maybe b) -> [a] -> [b]``
 
-    The mapMaybe function is a version of map which can throw out elements. In
-    particular, the functional argument returns something of type Maybe b. If
-    this is Nothing, no element is added on to the result list. If it is Just
-    b, then b is included in the result list.
+    A version of `map` using a function with a `Maybe` result.  If it is
+    ``Just b``, then `b` is included in the result list; otherwise, if it is
+    ``Nothing``, no element is included.
 
     """
+    from hask.lang.lazylist import L
     return L[(fromJust(b) for b in (f(a) for a in la) if isJust(b))]
+
+
+del Read, Show
+del H, sig, t
+del data, d, instance, deriving
+del Eq, Ord, Functor, Applicative, Monad
